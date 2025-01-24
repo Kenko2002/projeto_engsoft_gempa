c7170cadd326ef6097d27e14faa5fc4297efab5c
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2025-01-24 15:44:16 -0300

    Update login_tests.py

--- main/apps/socialentity/test/unit/login_tests.py
+++ main/apps/socialentity/test/unit/login_tests.py
@@ -25,11 +25,10 @@ def test_cadastro_and_login(client):
 
         assert response.status_code == status.HTTP_201_CREATED, f"Resposta inesperada: {response.content}" #Asserção completa
         assert Tecnico.objects.filter(email_contato=cadastro_data["email_contato"]).exists()
-        assert User.objects.filter(username=cadastro_data["email_contato"]).exists()
 
         # Parte do login (adicionada do exemplo anterior)
         login_data = {
-            "username": cadastro_data["email_contato"],
+            "username": cadastro_data["first_name"]+" "+cadastro_data["last_name"],
             "password": cadastro_data["password"],
         }
         url_login = reverse("login")  # Substitua 'login' pelo nome da URL de login
