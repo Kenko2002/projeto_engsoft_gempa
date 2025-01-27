8895205e7a560199b08772ccf44da7ef861ecb2c
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2025-01-27 17:12:37 -0300

    Update setor_busca_test.py

--- main/apps/encaminhamento/tests/setor_busca_test.py
+++ main/apps/encaminhamento/tests/setor_busca_test.py
@@ -41,3 +41,41 @@ def test_setor_por_funcao_api_view():
     assert response.json() == {'detail': 'Função não encontrada.'}
 
 
+
+
+
+
+
+@pytest.mark.django_db
+def test_setor_por_bairro_api_view():
+    """Testes para a API View SetorPorBairroAPIView."""
+    client = APIClient()
+
+    # Cria um Endereço e uma Unidade
+    endereco1 = Endereco.objects.create(bairro="Bairro A")
+    unidade1 = UnidadeOrganizacional.objects.create(nome="Unidade 1", enderecos=endereco1)
+    setor1 = SetorInstitucional.objects.create(nome="Setor 1", responsavel=None)
+    unidade1.setores_institucionais.add(setor1)
+
+    # Cria outro Endereço e Unidade em outro bairro
+    endereco2 = Endereco.objects.create(bairro="Bairro B")
+    unidade2 = UnidadeOrganizacional.objects.create(nome="Unidade 2",enderecos=endereco2)
+    setor2 = SetorInstitucional.objects.create(nome="Setor 2", responsavel=None)
+    unidade2.setores_institucionais.add(setor2)
+
+
+    # URL para teste
+    url = reverse('setor-bairro', kwargs={'bairro_nome': 'Bairro A'}) #'setor-por-bairro' deve ser o nome da sua url
+
+
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()['setores']) == 1  # Apenas setor1 deve ser retornado
+    assert response.json()['unidades'] == [unidade1.id]
+
+
+    #Teste sem bairro
+    url_sem_bairro = reverse('setor-bairro', kwargs={'bairro_nome': ''})
+    response = client.get(url_sem_bairro)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {'detail': 'Nome do bairro é obrigatório.'}
