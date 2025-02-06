71903244614987d39964073ac179edf783436cb6
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2025-02-06 11:01:48 -0300

    Update local.py

--- main/vepemamoduloum/settings/local.py
+++ main/vepemamoduloum/settings/local.py
@@ -2,7 +2,7 @@ from .base import *
 from decouple import config
 from os import environ
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-&we(0t(&@t(90rx$19tr3dms-3_4ngz6*6d=9=5ghz=ov#%^4^")
 DEBUG = os.environ.get("DEBUG", True) # Padrão True
 
 
