61436ae8a338dfee5ff9e84392b32a9be92da971
Renzo Fraga <130428183+Kenko2002@users.noreply.github.com>
2025-02-06 12:45:34 -0300

    Update production.py

--- main/vepemamoduloum/settings/production.py
+++ main/vepemamoduloum/settings/production.py
@@ -8,11 +8,11 @@ SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE_PRODUCTION'),
-        'NAME': config('DB_NAME_PRODUCTION'),
-        'USER': config('DB_USER_PRODUCTION'),
-        'PASSWORD': config('DB_PASSWORD_PRODUCTION'),
-        'HOST': config('DB_HOST_PRODUCTION'),
-        'PORT': config('DB_PORT_PRODUCTION'),
+        'ENGINE': config('DB_ENGINE_PRODUCTION', default='django.db.backends.sqlite3'),
+        'NAME': config('DB_NAME_PRODUCTION', default='db.sqlite3'),  # Or path if not in project root
+        'USER': config('DB_USER_PRODUCTION', default=''),  # Blank for SQLite
+        'PASSWORD': config('DB_PASSWORD_PRODUCTION', default=''), # Blank for SQLite
+        'HOST': config('DB_HOST_PRODUCTION', default=''),  # Blank for SQLite
+        'PORT': config('DB_PORT_PRODUCTION', default=''),  # Blank for SQLite
     }
 }
